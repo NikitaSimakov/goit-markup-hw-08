a04d56ff1988913af10a48b5ccd075b45a0376e6
NikitaSimakov <mishaaniskin@gmail.com>
2023-08-19 21:55:57 +0300

    added functionality filtering portfolio

---
+++ js/portfolioData.js
@@ -0,0 +1,214 @@
+const portfolioData = [
+  {
+    title: "Технокряк",
+    desc: "Веб-сайт",
+    text: "Ресурс предлагает комплексные предложения с разным уровнем функционала и сервисов. Все это позволит посетителю получить исчерпывающие сведения о компании&nbsp;или частном лице.",
+    src: {
+      src: "./images/portfolio/img-1_sm@1x.jpg",
+      src767:
+        "./images/portfolio/img-1_sm@1x.jpg 1x, ./images/portfolio/img-1_sm@2x.jpg 2x",
+      src768:
+        "./images/portfolio/img-1_md@1x.jpg 1x, ./images/portfolio/img-1_md@2x.jpg 2x",
+      src1200:
+        "./images/portfolio/img-1_lg@1x.jpg 1x, ./images/portfolio/img-1_lg@2x.jpg 2x",
+    },
+  },
+  {
+    title: "Постер New Orlean vs Golden Star",
+    desc: "Дизайн",
+    text: "Ресурс предлагает комплексные предложения с разным уровнем функционала и сервисов. Все это позволит посетителю получить исчерпывающие сведения о компании&nbsp;или частном лице.",
+    src: {
+      src: "./images/portfolio/img-2_sm@1x.jpg",
+      src767:
+        "./images/portfolio/img-2_sm@1x.jpg 1x, ./images/portfolio/img-2_sm@2x.jpg 2x",
+      src768:
+        "./images/portfolio/img-2_md@1x.jpg 1x, ./images/portfolio/img-2_md@2x.jpg 2x",
+      src1200:
+        "./images/portfolio/img-2_lg@1x.jpg 1x, ./images/portfolio/img-2_lg@2x.jpg 2x",
+    },
+  },
+  {
+    title: "Ресторан Seafood",
+    desc: "Приложение",
+    text: "Ресурс предлагает комплексные предложения с разным уровнем функционала и сервисов. Все это позволит посетителю получить исчерпывающие сведения о компании&nbsp;или частном лице.",
+    src: {
+      src: "./images/portfolio/img-3_sm@1x.jpg",
+      src767:
+        "./images/portfolio/img-3_sm@1x.jpg 1x, ./images/portfolio/img-3_sm@2x.jpg 2x",
+      src768:
+        "./images/portfolio/img-3_md@1x.jpg 1x, ./images/portfolio/img-3_md@2x.jpg 2x",
+      src1200:
+        "./images/portfolio/img-3_lg@1x.jpg 1x, ./images/portfolio/img-3_lg@2x.jpg 2x",
+    },
+  },
+  {
+    title: "Проект Prime",
+    desc: "Маркетинг",
+    text: "Ресурс предлагает комплексные предложения с разным уровнем функционала и сервисов. Все это позволит посетителю получить исчерпывающие сведения о компании&nbsp;или частном лице.",
+    src: {
+      src: "./images/portfolio/img-4_sm@1x.jpg",
+      src767:
+        "./images/portfolio/img-4_sm@1x.jpg 1x, ./images/portfolio/img-4_sm@2x.jpg 2x",
+      src768:
+        "./images/portfolio/img-4_md@1x.jpg 1x, ./images/portfolio/img-4_md@2x.jpg 2x",
+      src1200:
+        "./images/portfolio/img-4_lg@1x.jpg 1x, ./images/portfolio/img-4_lg@2x.jpg 2x",
+    },
+  },
+  {
+    title: "Проект Boxes",
+    desc: "Приложение",
+    text: "Ресурс предлагает комплексные предложения с разным уровнем функционала и сервисов. Все это позволит посетителю получить исчерпывающие сведения о компании&nbsp;или частном лице.",
+    src: {
+      src: "./images/portfolio/img-5_sm@1x.jpg",
+      src767:
+        "./images/portfolio/img-5_sm@1x.jpg 1x, ./images/portfolio/img-5_sm@2x.jpg 2x",
+      src768:
+        "./images/portfolio/img-5_md@1x.jpg 1x, ./images/portfolio/img-5_md@2x.jpg 2x",
+      src1200:
+        "./images/portfolio/img-5_lg@1x.jpg 1x, ./images/portfolio/img-5_lg@2x.jpg 2x",
+    },
+  },
+  {
+    title: "Inspiration has no Borders",
+    desc: "Веб-сайт",
+    text: "Ресурс предлагает комплексные предложения с разным уровнем функционала и сервисов. Все это позволит посетителю получить исчерпывающие сведения о компании&nbsp;или частном лице.",
+    src: {
+      src: "./images/portfolio/img-6_sm@1x.jpg",
+      src767:
+        "./images/portfolio/img-6_sm@1x.jpg 1x, ./images/portfolio/img-6_sm@2x.jpg 2x",
+      src768:
+        "./images/portfolio/img-6_md@1x.jpg 1x, ./images/portfolio/img-6_md@2x.jpg 2x",
+      src1200:
+        "./images/portfolio/img-6_lg@1x.jpg 1x, ./images/portfolio/img-6_lg@2x.jpg 2x",
+    },
+  },
+  {
+    title: "Издание Limited Edition",
+    desc: "Дизайн",
+    text: "Ресурс предлагает комплексные предложения с разным уровнем функционала и сервисов. Все это позволит посетителю получить исчерпывающие сведения о компании&nbsp;или частном лице.",
+    src: {
+      src: "./images/portfolio/img-7_sm@1x.jpg",
+      src767:
+        "./images/portfolio/img-7_sm@1x.jpg 1x, ./images/portfolio/img-7_sm@2x.jpg 2x",
+      src768:
+        "./images/portfolio/img-7_md@1x.jpg 1x, ./images/portfolio/img-7_md@2x.jpg 2x",
+      src1200:
+        "./images/portfolio/img-7_lg@1x.jpg 1x, ./images/portfolio/img-7_lg@2x.jpg 2x",
+    },
+  },
+  {
+    title: "Проект LAB",
+    desc: "Маркетинг",
+    text: "Ресурс предлагает комплексные предложения с разным уровнем функционала и сервисов. Все это позволит посетителю получить исчерпывающие сведения о компании&nbsp;или частном лице.",
+    src: {
+      src: "./images/portfolio/img-8_sm@1x.jpg",
+      src767:
+        "./images/portfolio/img-8_sm@1x.jpg 1x, ./images/portfolio/img-8_sm@2x.jpg 2x",
+      src768:
+        "./images/portfolio/img-8_md@1x.jpg 1x, ./images/portfolio/img-8_md@2x.jpg 2x",
+      src1200:
+        "./images/portfolio/img-8_lg@1x.jpg 1x, ./images/portfolio/img-8_lg@2x.jpg 2x",
+    },
+  },
+  {
+    title: "Growing Business",
+    desc: "Приложение",
+    text: "Ресурс предлагает комплексные предложения с разным уровнем функционала и сервисов. Все это позволит посетителю получить исчерпывающие сведения о компании&nbsp;или частном лице.",
+    src: {
+      src: "./images/portfolio/img-9_sm@1x.jpg",
+      src767:
+        "./images/portfolio/img-9_sm@1x.jpg 1x, ./images/portfolio/img-9_sm@2x.jpg 2x",
+      src768:
+        "./images/portfolio/img-9_md@1x.jpg 1x, ./images/portfolio/img-9_md@2x.jpg 2x",
+      src1200:
+        "./images/portfolio/img-9_lg@1x.jpg 1x, ./images/portfolio/img-9_lg@2x.jpg 2x",
+    },
+  },
+];
+
+const workList = document.querySelector("[data-work-list]");
+const buttonList = document.querySelector(".button__list");
+
+const filteredPortfolio = (type = "Все") =>
+  portfolioData.filter((card, _, array) => {
+    if (type === "Все") return array;
+    if (type === "Приложения") type = "Приложение";
+    if (type === "Веб-сайты") type = "Веб-сайт";
+    return card.desc === type;
+  });
+
+let staticMarkup = filteredPortfolio("Все");
+const markup = (array = portfolioData) => {
+  return (staticMarkup = array
+    .map(
+      (card) => `<li class="works__item">
+    <div class="works__img">
+      <a href="/">
+        <picture>
+          <source media="(min-width: 1200px)"
+          srcset=${card.src.src1200}
+          />
+          <source media="(min-width: 768px)"
+          srcset=${card.src.src768}
+          />
+          <source media="(max-width: 767px)"
+          srcset=${card.src.src767}
+          />
+          <img class="portfolio-img" src=${card.src.src} alt="Изображение" />
+        </picture>
+        <div class="overlay">
+          <p class="overlay__text">${card.text}
+          </p>
+        </div>
+      </a>
+    </div>
+    <div class="border">
+      <h2 class="works__title">${card.desc}</h2>
+      <p class="works__text">${card.title}</p>
+    </div>
+    </li>`
+    )
+    .join(" "));
+};
+markup();
+
+const handleButtonClick = (event) => {
+  if (event.target.nodeName !== "BUTTON") return;
+  workList.innerHTML = "";
+  const filteredArr = filteredPortfolio(event.target.innerText);
+  staticMarkup = markup(filteredArr);
+  workList.insertAdjacentHTML("beforeend", staticMarkup);
+  return staticMarkup;
+};
+buttonList.addEventListener("click", handleButtonClick);
+workList.insertAdjacentHTML("beforeend", staticMarkup);
+// const razmetka = markup(filteredPortfolio(handleButtonClick));
+// (
+//     (card) => `<li class="works__item">
+//       <div class="works__img">
+//         <a href="/">
+//           <picture>
+//             <source media="(min-width: 1200px)"
+//             srcset=${card.src.src1200}
+//             />
+//             <source media="(min-width: 768px)"
+//             srcset=${card.src.src768}
+//             />
+//             <source media="(max-width: 767px)"
+//             srcset=${card.src.src767}
+//             />
+//             <img class="portfolio-img" src=${card.src.src} alt="Изображение" />
+//           </picture>
+//           <div class="overlay">
+//             <p class="overlay__text">${card.text}
+//             </p>
+//           </div>
+//         </a>
+//       </div>
+//       <div class="border">
+//         <h2 class="works__title">${card.desc}</h2>
+//         <p class="works__text">${card.title}</p>
+//       </div>
+//       </li>`
+//   )
